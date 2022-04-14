96a17d21258bd950f83efc0b20031d8b1b3b2565
Jepunen <62857023+Jepunen@users.noreply.github.com>
2022-04-14 13:29:02 +0300

    Add login fragment and java files

--- app/src/main/java/com/example/vko11v3/LogInFragment.java
+++ app/src/main/java/com/example/vko11v3/LogInFragment.java
@@ -21,6 +21,8 @@ public class LogInFragment extends Fragment{
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+
+
     }
 
 }
